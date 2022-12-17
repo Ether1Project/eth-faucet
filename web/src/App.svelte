3b14dd4d00e046e2fce5ae31dfd0ea43418496d5
legacytrx <legacytrx@atheios.org>
2022-12-17 23:04:31 +0100

    Design update

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -5,6 +5,13 @@
 </script>
 
 <svelte:head>
+  <meta name="twitter:card" content="summary" />
+  <meta name="twitter:site" content="@ethoprotocol" />
+  <meta name="twitter:creator" content="@ethoprotocol" />
+  <meta property="og:url" content="https://testnetfaucet.ethoprotocol.com" />
+  <meta property="og:title" content="A faucet to provide ETHO HC coins - the testnet coins of Etho Protocol" />
+  <meta property="og:description" content="Etho HC Testnet work coins can be obtained here in order to interact with the test net." />
+  <meta property="og:image" content="https://testnetfaucet.ethoprotocol.com/favicon.png" />
   <link
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
     rel="stylesheet"
